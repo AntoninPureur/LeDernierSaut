84259435ba24adae9539f895a780244de840758c
DevilLouis <ninjapitch@gmail.com>
2024-03-12 02:32:39 +0100

    left&right movement & gravity
    relative height of controllers moves the player to the left/right

---
+++ Assets/Scripts/ControllerHeightMovement.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingingArmMotion : MonoBehaviour
+{
+    // Game Objects
+    [SerializeField] private GameObject LeftHand;
+    [SerializeField] private GameObject RightHand;
+    [SerializeField] private GameObject MainCamera;
+    [SerializeField] private GameObject SidewayDirection;
+
+    //Vector3 Positions
+    [SerializeField] private Vector3 PositionPreviousFrameLeftHand;
+    [SerializeField] private Vector3 PositionPreviousFrameRightHand;
+    [SerializeField] private Vector3 PlayerPositionPreviousFrame;
+    [SerializeField] private Vector3 PlayerPositionCurrentFrame;
+    [SerializeField] private Vector3 PositionCurrentFrameLeftHand;
+    [SerializeField] private Vector3 PositionCurrentFrameRightHand;
+
+    //Speed
+    [SerializeField] private float Speed = 70;
+    [SerializeField] private float HandSpeed;
+
+    void Start()
+    {
+        PlayerPositionPreviousFrame = transform.position; //set current positions
+        PositionPreviousFrameLeftHand = LeftHand.transform.position; //set previous positions
+        PositionPreviousFrameRightHand = RightHand.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get forward direction from the center eye camera and set it to the forward direction object
+        float yRotation = MainCamera.transform.eulerAngles.y;
+        SidewayDirection.transform.eulerAngles = new Vector3(0,yRotation, 0);
+
+        // get positons of hands
+        PositionCurrentFrameLeftHand = LeftHand.transform.position;
+        PositionCurrentFrameRightHand = RightHand.transform.position;
+
+        // position of player
+        PlayerPositionCurrentFrame = transform.position;
+
+        // get distance the hands and player has moved from last frame
+        var heightDifferenceHands = PositionCurrentFrameLeftHand.y - PositionCurrentFrameRightHand.y;
+        
+
+        if (Time.timeSinceLevelLoad > 1f)
+        {
+            transform.position += SidewayDirection.transform.right * heightDifferenceHands * Speed * Time.deltaTime;
+        }
+        // set previous position of hands for next frame
+        PositionPreviousFrameLeftHand = PositionCurrentFrameLeftHand;
+        PositionPreviousFrameRightHand = PositionCurrentFrameRightHand;
+        // set player position previous frame
+        PlayerPositionPreviousFrame = PlayerPositionCurrentFrame;
+    }
+}
+
