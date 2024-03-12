db5e5970440abf0ef547fa58f32f65eb29ed8bcd
DevilLouis <ninjapitch@gmail.com>
2024-03-12 04:10:06 +0100

    Added the forward/backward movement
    The forward/backward movement is calculated with the distance between controller on the x axis

--- Assets/Scripts/ControllerHeightMovement.cs
+++ Assets/Scripts/ControllerHeightMovement.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static System.Math;
 
-public class SwingingArmMotion : MonoBehaviour
+public class ControllerHeightMovement : MonoBehaviour
 {
     // Game Objects
     [SerializeField] private GameObject LeftHand;
     [SerializeField] private GameObject RightHand;
     [SerializeField] private GameObject MainCamera;
     [SerializeField] private GameObject SidewayDirection;
+    [SerializeField] private GameObject ForwardDirection;
 
     //Vector3 Positions
     [SerializeField] private Vector3 PositionPreviousFrameLeftHand;
@@ -17,13 +19,23 @@ public class SwingingArmMotion : MonoBehaviour
     [SerializeField] private Vector3 PlayerPositionCurrentFrame;
     [SerializeField] private Vector3 PositionCurrentFrameLeftHand;
     [SerializeField] private Vector3 PositionCurrentFrameRightHand;
+    [SerializeField] private double XDistanceHands;
+    [SerializeField] private double YDistanceHands;
+    [SerializeField] private bool IsGrounded;
 
     //Speed
-    [SerializeField] private float Speed = 70;
-    [SerializeField] private float HandSpeed;
+    [SerializeField] private float ForwardSpeed = 10;
+    [SerializeField] private float BackwardSpeed = 10;
+    [SerializeField] private float SidewardSpeed = 6;
+
+    //x Distance where the movement is neither forward or backward
+    [SerializeField] private double RefXDistance = 0.15;
 
     void Start()
     {
+       
+        bool isGrounded = Physics.Raycast(transform.position, -Vector3.up, 0.1f);
+
         PlayerPositionPreviousFrame = transform.position; //set current positions
         PositionPreviousFrameLeftHand = LeftHand.transform.position; //set previous positions
         PositionPreviousFrameRightHand = RightHand.transform.position;
@@ -32,9 +44,13 @@ public class SwingingArmMotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        bool isGrounded = Physics.Raycast(transform.position, -Vector3.up, 0.1f);
+
         // get forward direction from the center eye camera and set it to the forward direction object
         float yRotation = MainCamera.transform.eulerAngles.y;
-        SidewayDirection.transform.eulerAngles = new Vector3(0,yRotation, 0);
+        SidewayDirection.transform.eulerAngles = new Vector3(0, yRotation, 0);
+        ForwardDirection.transform.eulerAngles = new Vector3(0, yRotation, 0);
 
         // get positons of hands
         PositionCurrentFrameLeftHand = LeftHand.transform.position;
@@ -43,13 +59,30 @@ public class SwingingArmMotion : MonoBehaviour
         // position of player
         PlayerPositionCurrentFrame = transform.position;
 
-        // get distance the hands and player has moved from last frame
-        var heightDifferenceHands = PositionCurrentFrameLeftHand.y - PositionCurrentFrameRightHand.y;
-        
+        // get distances between controllers
+        var yDistanceHands = PositionCurrentFrameLeftHand.y - PositionCurrentFrameRightHand.y;
+        var xDistanceHands =Abs(PositionCurrentFrameLeftHand.x- PositionCurrentFrameRightHand.x);
+
+
+        XDistanceHands = xDistanceHands;
+        YDistanceHands = yDistanceHands;
+        IsGrounded = isGrounded;
+
+
 
         if (Time.timeSinceLevelLoad > 1f)
         {
-            transform.position += SidewayDirection.transform.right * heightDifferenceHands * Speed * Time.deltaTime;
+            transform.position += SidewayDirection.transform.right * yDistanceHands * SidewardSpeed * Time.deltaTime;
+            if (xDistanceHands > RefXDistance+0.1) 
+            {
+                transform.position += -ForwardDirection.transform.forward * (float)(yDistanceHands-RefXDistance) * ForwardSpeed * Time.deltaTime;
+            }
+            if (xDistanceHands < RefXDistance - 0.1)
+            {
+               transform.position += ForwardDirection.transform.forward * (float)(yDistanceHands - RefXDistance) * BackwardSpeed * Time.deltaTime;
+            }
+
+            //
         }
         // set previous position of hands for next frame
         PositionPreviousFrameLeftHand = PositionCurrentFrameLeftHand;
