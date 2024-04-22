158e960256c38ffacd20a51b9c9e94e4917794cf
AntoninPureur <132441670+AntoninPureur@users.noreply.github.com>
2024-04-22 19:36:03 -0400

    NetworkBehavior

--- Assets/Scripts/ScriptTP.cs
+++ Assets/Scripts/ScriptTP.cs
@@ -1,8 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
-public class ScriptTP : MonoBehaviour
+public class ScriptTP : NetworkBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,7 @@ public class ScriptTP : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         other.transform.position = new Vector3(0, 400, 0);
+        GetComponent<MeshRenderer>().enabled = true;
     }
 
 }
