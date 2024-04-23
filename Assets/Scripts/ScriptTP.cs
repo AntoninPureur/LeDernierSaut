0db12da64e9170832f2d85cca1ae750a3b7b745c
AntoninPureur <132441670+AntoninPureur@users.noreply.github.com>
2024-04-22 22:31:44 -0400

    Mise en place fonction

--- Assets/Scripts/ScriptTP.cs
+++ Assets/Scripts/ScriptTP.cs
@@ -9,12 +9,15 @@ public class ScriptTP : NetworkBehaviour
 {
     // Référence au composant MeshRenderer
     private MeshRenderer meshRenderer;
+    public int etat;
+
 
     // Start is called before the first frame update
     void Start()
     {
         // Récupérer le composant MeshRenderer au démarrage
         meshRenderer = GetComponent<MeshRenderer>();
+        etat = 0;
     }
 
     // Update is called once per frame
@@ -25,12 +28,24 @@ public class ScriptTP : NetworkBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        // Vérifier si le script est exécuté sur le serveur
-        // Changer la position de l'objet pour tous les joueurs
-        other.transform.position = new Vector3(0, 400, 0);
 
-        // Activer la texture pour tous les joueurs
-        RpcActivateTextureServerRpc();
+        if (etat == 0)
+        {
+            // Vérifier si le script est exécuté sur le serveur
+            // Changer la position de l'objet pour tous les joueurs
+            other.transform.position = new Vector3(0, 400, 0);
+
+            // Activer la texture pour tous les joueurs
+            RpcActivateTextureServerRpc();
+        }
+
+        if (etat == 1)
+        {
+
+            WinScreenServerRpc();
+        }
+
+        etat++;
     }
 
     // Méthode pour activer la texture sur tous les clients via un RPC
@@ -41,7 +56,14 @@ public class ScriptTP : NetworkBehaviour
         RpcActivateTextureClientRpc();
     }
 
-    
+    [ServerRpc(RequireOwnership = false)]
+    void WinScreenServerRpc()
+    {
+        // Appeler une méthode sur tous les clients pour activer la texture
+        WinScreenClientRpc();
+    }
+
+
     // Méthode RPC client pour activer la texture sur tous les clients
     [ClientRpc]
     void RpcActivateTextureClientRpc()
@@ -49,4 +71,11 @@ public class ScriptTP : NetworkBehaviour
         // Activer le composant MeshRenderer
         meshRenderer.enabled = true;
     }
+
+    [ClientRpc]
+    void WinScreenClientRpc()
+    {
+
+    }
+
 }
